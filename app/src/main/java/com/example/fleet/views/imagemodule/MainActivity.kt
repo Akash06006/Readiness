49944e7cb8b1474456431795be6e4d82030a4c6a
2926 <ansarisaira@seasiainfotech.com>
2020-06-15 17:38:47 +0530

    Saira's Commit

--- app/src/main/java/com/example/fleet/views/imagemodule/MainActivity.kt
+++ app/src/main/java/com/example/fleet/views/imagemodule/MainActivity.kt
@@ -1,27 +1,27 @@
 package com.e.dummyproject
 
 import android.os.Build
-import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
 import androidx.annotation.RequiresApi
-import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.example.fleet.R
 import com.example.fleet.application.MyApplication
+import com.example.fleet.databinding.ActivityMainBinding
 import com.example.fleet.model.CategoriesType
+import com.example.fleet.utils.BaseActivity
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     var btnUpload : Button? = null
     var image : ImageView? = null
+    private lateinit var binding : ActivityMainBinding
 
     @RequiresApi(Build.VERSION_CODES.M)
-    override fun onCreate(savedInstanceState : Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun initViews() {
         image = findViewById(R.id.ivImage);
         btnUpload = findViewById(R.id.btnUpload);
+        binding = viewDataBinding as ActivityMainBinding
 
         if (intent.getStringExtra("uri") != null) {
             Glide.with(this)
@@ -30,33 +30,25 @@ class MainActivity : AppCompatActivity() {
         }
         btnUpload!!.setOnClickListener {
             var list = ArrayList<CategoriesType.Images>()
-//
-////           imagies.imageName=intent.getStringExtra("name")
-//////          /
-//////            list.add(imagies)
-
             val imagies = CategoriesType.Images()
             imagies.imageName = intent.getStringExtra("categoriesId")
             imagies.imagePath = intent.getStringExtra("uri")
 
+            for (i in 0..MyApplication.instance.categoriesList!!.size - 1) {
 
-            //val index = MyApplication.instance.categoriesList!!.find(intent.getStringExtra("categoriesId")) // pass value
-            //val index: Event? =  MyApplication.instance.categoriesList!!.find { it.categoriesId == intent.getStringExtra("categoriesId") }
-
-           // val imageModel = MyApplication.instance.categoriesList!![index]
-           // val images = MyApplication.instance.categoriesList!![index].images
-         //   images!!.add(imagies)
-            //MyApplication.instance.categoriesList!!.set(index, imageModel)
-
-
-//            for (i in 0..MyApplication.instance.categoriesList!!.size-1){
-//
-//                if(MyApplication.instance.categoriesList!!.get(i).categoryId.equals(intent.getStringExtra("categoriesId"))){
-//                    MyApplication.instance.categoriesList!!.set(i,imageModel)
-//                }
-//            }
+                if (MyApplication.instance.categoriesList!!.get(i).categoryId.equals(intent.getStringExtra("categoriesId"))) {
+                    val imageModel = MyApplication.instance.categoriesList!![i]
+                    val images = MyApplication.instance.categoriesList!![i].images
+                    images!!.add(imagies)
+                    MyApplication.instance.categoriesList!!.set(i, imageModel)
+                }
+            }
 
             finish()
         }
+    }
+
+    override fun getLayoutId() : Int {
+        return R.layout.activity_main
     }
 }
