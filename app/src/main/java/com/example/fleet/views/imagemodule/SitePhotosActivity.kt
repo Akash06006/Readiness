5606eb1f83cebdaba99bcc5d8f1f0dc02a76f75d
2926 <ansarisaira@seasiainfotech.com>
2020-06-15 16:23:28 +0530

    Saira's Commit

--- app/src/main/java/com/example/fleet/views/imagemodule/SitePhotosActivity.kt
+++ app/src/main/java/com/example/fleet/views/imagemodule/SitePhotosActivity.kt
@@ -1,57 +1,46 @@
 package com.e.dummyproject
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import android.widget.RadioButton
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
-import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.fleet.R
 import com.example.fleet.adapters.ImageCategories
+import com.example.fleet.databinding.ActivitySitePhotosBinding
+import com.example.fleet.databinding.ActivitySurveyBinding
 import com.example.fleet.model.ImageCategoriesResponse
 import com.example.fleet.utils.BaseActivity
 import com.example.fleet.viewmodels.ImageCategoryModel
+import com.example.fleet.viewmodels.SiteInfoViewModel
 
-class SitePhotosActivity : BaseActivity(), View.OnClickListener{
-    var btnSubmt: Button? = null
+class SitePhotosActivity : BaseActivity() {
     private lateinit var imageCaegoryViewModel : ImageCategoryModel
+    private lateinit var binding : ActivitySitePhotosBinding
 
-//    var radioGrop1: RadioGroup? = null
-//    var radioGrop2: RadioGroup? = null
-//    var radioGrop3: RadioGroup? = null
-
-    var rvExterior:RadioButton?=null
-    var rvinterior:RadioButton?=null
-    var parking:RadioButton?=null
-    var ivCenter:RadioButton?=null
-    var rbStreet:RadioButton?=null
-    var adapter:ImageCategories?=null
-    var rvPublic:RecyclerView?=null
-    var categoriesList:ArrayList<ImageCategoriesResponse.ResultData>?=null
+    var adapter : ImageCategories? = null
+    var rvPublic : RecyclerView? = null
+    var categoriesList : ArrayList<ImageCategoriesResponse.ResultData>? = null
     override fun initViews() {
-        categoriesList=ArrayList()
 
-
-        btnSubmt = findViewById(R.id.btnSubmit)
-        rvPublic = findViewById(R.id.rvPublic)
-        btnSubmt!!.setOnClickListener {
-            var intent = Intent(this, ImageListActivity::class.java)
-            startActivity(intent)
-        }
+        binding = viewDataBinding as ActivitySitePhotosBinding
+        categoriesList = ArrayList()
+        rvPublic =binding.rvPublic
         setadapter()
         imageCaegoryViewModel = ViewModelProviders.of(this).get(ImageCategoryModel::class.java)
+        binding.imageCaegoryViewModel = imageCaegoryViewModel
+
         imageCaegoryViewModel.getData().observe(this,
             Observer<ImageCategoriesResponse> { response->
                 stopProgressDialog()
                 if (response != null) {
                     val message = response.message
 
-                    if(response.statusCode.equals("200")){
-                        categoriesList=response.resultData
+                    if (response.statusCode.equals("200")) {
+                        categoriesList = response.resultData
 
                         adapter!!.setList(categoriesList)
                     }
@@ -66,25 +55,38 @@ class SitePhotosActivity : BaseActivity(), View.OnClickListener{
             }
         })
 
+        imageCaegoryViewModel.isClick().observe(
+            this, Observer<String>(function =
+            fun(it : String?) {
+
+                when (it) {
+
+                    "btnSubmt" -> {
+                       // startActivity(Intent(this, SitePhotosActivity::class.java))
+
+                    }
+                }
+
+            })
+        )
+
+
+
     }
 
     override fun getLayoutId() : Int {
-      return  R.layout.activity_site_photos
+        return R.layout.activity_site_photos
     }
 
 
-
     fun setadapter() {
-        adapter=ImageCategories( this,categoriesList)
-        rvPublic!!.setLayoutManager( GridLayoutManager(this, 2));
-
+        adapter = ImageCategories(this, categoriesList)
+        rvPublic!!.setLayoutManager(GridLayoutManager(this, 2));
         rvPublic!!.adapter = adapter
     }
 
 
-    override fun onClick(p0: View?) {
 
-       }
-    }
+}
 
 
